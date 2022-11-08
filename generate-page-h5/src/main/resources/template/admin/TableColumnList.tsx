300ac8e921bcacbdaa1458545699feed59827699
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-08 23:39:07 +0800

    【i】【CmcOrg】

--- generate-page-h5/src/main/resources/template/admin/TableColumnList.tsx
+++ generate-page-h5/src/main/resources/template/admin/TableColumnList.tsx
@@ -7,7 +7,7 @@ const TableColumnList = (currentForm: React.MutableRefObject<AdminInsertOrUpdate
         title: '序号',
         dataIndex: 'index',
         valueType: 'index',
-        width: 50,
+        width: 90,
     },
 AdminTableJson
     {
